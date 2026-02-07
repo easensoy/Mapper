f9fcbb21c2893104bae186f95208d93abdc4bd81
easensoy <eminalpersensoy@gmail.com>
2026-02-07 21:24:08 +0000

    Add Create Configuration module

---
+++ CodeGen/CodeGen/Create Configuration/MapperConfig.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Xml;
+
+namespace VueOneMapper.Configuration
+{
+    /// <summary>
+    /// Configuration settings for the VueOne Mapper
+    /// </summary>
+    public class MapperConfig
+    {
+        public string ControlXmlPath { get; set; }
+        public string TemplateDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+        public string EAEProjectPath { get; set; }
+
+        /// <summary>
+        /// Load configuration from config.json
+        /// </summary>
+        public static MapperConfig Load(string configPath = "config.json")
+        {
+            if (!File.Exists(configPath))
+            {
+                // Create default config
+                var defaultConfig = new MapperConfig
+                {
+                    ControlXmlPath = @"C:\VueOne\component\Pusher\Control.xml",
+                    TemplateDirectory = @"Templates",
+                    OutputDirectory = @"Output",
+                    EAEProjectPath = @"C:\EAE\SMC_Rig_Expo\Composite"
+                };
+
+                // Save default config
+                string json = JsonConvert.SerializeObject(defaultConfig, Formatting.Indented);
+                File.WriteAllText(configPath, json);
+
+                return defaultConfig;
+            }
+
+            string existingJson = File.ReadAllText(configPath);
+            return JsonConvert.DeserializeObject<MapperConfig>(existingJson);
+        }
+
+        /// <summary>
+        /// Save configuration to config.json
+        /// </summary>
+        public void Save(string configPath = "config.json")
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(configPath, json);
+        }
+    }
+}
