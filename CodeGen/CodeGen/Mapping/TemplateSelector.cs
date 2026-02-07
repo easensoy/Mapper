6e9eef0e16f3882f5f74e0df885f3c8cb2a9db40
easensoy <eminalpersensoy@gmail.com>
2026-02-07 21:48:39 +0000

    Update TemplateSelector

--- CodeGen/CodeGen/Mapping/TemplateSelector.cs
+++ CodeGen/CodeGen/Mapping/TemplateSelector.cs
@@ -1,40 +1,28 @@
-﻿using VueOneMapper.Models;
+﻿using CodeGen.Models;
 
-namespace VueOneMapper.Mapping
+namespace CodeGen.Mapping
 {
-    /// <summary>
-    /// Selects the appropriate IEC 61499 template based on VueOne component type
-    /// </summary>
     public class TemplateSelector
     {
-        public FBTemplate SelectTemplate(VueOneComponent component)
+        public FBTemplate? SelectTemplate(VueOneComponent component)
         {
-            // For Tuesday demo: Focus on Five-State Actuator
-            if (component.Type == "Actuator" && component.States.Count == 5)
+            return (component.Type, component.States.Count) switch
             {
-                return new FBTemplate
-                {
-                    TemplateName = "five_state_actuator.fbt",
-                    TemplateFilePath = "five_state_actuator.fbt",
-                    ExpectedStateCount = 5,
-                    ComponentType = "Actuator"
-                };
-            }
+                ("Actuator", 5) => CreateTemplate("five_state_actuator.fbt", 5, "Actuator"),
+                ("Sensor", 2) => CreateTemplate("two_state_sensor.fbt", 2, "Sensor"),
+                _ => null
+            };
+        }
 
-            // Future: Two-State Sensor
-            if (component.Type == "Sensor" && component.States.Count == 2)
+        private FBTemplate CreateTemplate(string fileName, int stateCount, string componentType)
+        {
+            return new FBTemplate
             {
-                return new FBTemplate
-                {
-                    TemplateName = "two_state_sensor.fbt",
-                    TemplateFilePath = "two_state_sensor.fbt",
-                    ExpectedStateCount = 2,
-                    ComponentType = "Sensor"
-                };
-            }
-
-            // No matching template
-            return null;
+                TemplateName = fileName,
+                TemplateFilePath = fileName,
+                ExpectedStateCount = stateCount,
+                ComponentType = componentType
+            };
         }
     }
 }
