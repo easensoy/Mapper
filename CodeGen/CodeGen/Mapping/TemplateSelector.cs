9205a6f6ac881e71bdd95b194af5e616a1e16335
easensoy <eminalpersensoy@gmail.com>
2026-02-07 20:58:20 +0000

    Add Mapping module

---
+++ CodeGen/CodeGen/Mapping/TemplateSelector.cs
@@ -0,0 +1,41 @@
+﻿using CodeGen.Models;
+using VueOneMapper.Models;
+
+namespace VueOneMapper.Mapping
+{
+    /// <summary>
+    /// Selects the appropriate IEC 61499 template based on VueOne component type
+    /// </summary>
+    public class TemplateSelector
+    {
+        public FBTemplate SelectTemplate(VueOneComponent component)
+        {
+            // For Tuesday demo: Focus on Five-State Actuator
+            if (component.Type == "Actuator" && component.States.Count == 5)
+            {
+                return new FBTemplate
+                {
+                    TemplateName = "five_state_actuator.fbt",
+                    TemplateFilePath = "five_state_actuator.fbt",
+                    ExpectedStateCount = 5,
+                    ComponentType = "Actuator"
+                };
+            }
+
+            // Future: Two-State Sensor
+            if (component.Type == "Sensor" && component.States.Count == 2)
+            {
+                return new FBTemplate
+                {
+                    TemplateName = "two_state_sensor.fbt",
+                    TemplateFilePath = "two_state_sensor.fbt",
+                    ExpectedStateCount = 2,
+                    ComponentType = "Sensor"
+                };
+            }
+
+            // No matching template
+            return null;
+        }
+    }
+}
