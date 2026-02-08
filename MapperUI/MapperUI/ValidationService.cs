d78c72e994cb1d1f7e5cf380e1acc605443c260a
easensoy <eminalpersensoy@gmail.com>
2026-02-08 02:55:23 +0000

    Add ValidationService

---
+++ MapperUI/MapperUI/ValidationService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using CodeGen.IO;
+using CodeGen.Validation;
+using CodeGen.Models;
+
+namespace MapperUI.Services
+{
+    public class ValidationService
+    {
+        public async Task<ValidationResult> ValidateControlXml(string xmlPath)
+        {
+            return await Task.Run(() =>
+            {
+                var xmlReader = new ControlXmlReader();
+                var component = xmlReader.ReadComponent(xmlPath);
+
+                var validator = new ComponentValidator();
+                return validator.Validate(component);
+            });
+        }
+
+        public async Task<VueOneComponent> ReadComponent(string xmlPath)
+        {
+            return await Task.Run(() =>
+            {
+                var xmlReader = new ControlXmlReader();
+                return xmlReader.ReadComponent(xmlPath);
+            });
+        }
+    }
+}
