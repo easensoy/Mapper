7ab6de3e24ec2442c84468a57df0a31c3738719a
easensoy <eminalpersensoy@gmail.com>
2026-03-01 22:31:25 +0000

    Add SystemLayoutModels

---
+++ CodeGen/CodeGen/Models/SystemLayoutModels.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MapperUI.Services
+{
+    public class SystemInjectionResult
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public List<string> InjectedFBs { get; set; } = new List<string>();
+        public List<string> SkippedFBs { get; set; } = new List<string>();
+        public List<string> UnsupportedComponents { get; set; } = new List<string>();
+        public string SyslayPath { get; set; } = string.Empty;
+        public string SysresPath { get; set; } = string.Empty;
+    }
+}
