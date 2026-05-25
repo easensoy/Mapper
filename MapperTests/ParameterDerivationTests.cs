bb7b43324e4c5dd4e690da7c80c68830f3fdadd0
easensoy <eminalpersensoy@gmail.com>
2026-05-25 10:17:41 +0100

    Update ParameterDerivationTests usings for modular refactor

--- MapperTests/ParameterDerivationTests.cs
+++ MapperTests/ParameterDerivationTests.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using CodeGen.IO;
 using CodeGen.Models;
 using CodeGen.Translation;
-using MapperUI.Services;
+using CodeGen.Devices.M262;
+using CodeGen.Devices.M580;
+using CodeGen.Devices.Shared;
+using CodeGen.Services;
 using Xunit;
 
 namespace MapperTests
