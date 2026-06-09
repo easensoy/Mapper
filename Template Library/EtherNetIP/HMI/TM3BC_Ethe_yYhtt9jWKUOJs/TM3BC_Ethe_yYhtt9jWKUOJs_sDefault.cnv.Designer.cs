94ea7af8e93c56bf4fbe34546b0b157a4f232f39
easensoy <eminalpersensoy@gmail.com>
2026-06-09 18:21:11 +0100

    Add TM3BC EtherNetIP canvas designer

---
+++ Template Library/EtherNetIP/HMI/TM3BC_Ethe_yYhtt9jWKUOJs/TM3BC_Ethe_yYhtt9jWKUOJs_sDefault.cnv.Designer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.TM3BC_Ethe_yYhtt9jWKUOJs
+{
+	/// <summary>
+	/// Summary description for sDefault.
+	/// </summary>
+	partial class sDefault
+	{
+
+		#region Component Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// sDefault
+			// 
+			this.Name = "sDefault";
+			this.SymbolSize = new System.Drawing.Size(200, 368);
+
+		}
+		#endregion
+	}
+}
