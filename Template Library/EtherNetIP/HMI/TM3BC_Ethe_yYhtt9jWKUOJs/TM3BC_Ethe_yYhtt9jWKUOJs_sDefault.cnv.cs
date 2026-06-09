bcd01c1c08e228586b625b702fa74e19b707368b
easensoy <eminalpersensoy@gmail.com>
2026-06-09 18:21:10 +0100

    Add TM3BC EtherNetIP default canvas

---
+++ Template Library/EtherNetIP/HMI/TM3BC_Ethe_yYhtt9jWKUOJs/TM3BC_Ethe_yYhtt9jWKUOJs_sDefault.cnv.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.TM3BC_Ethe_yYhtt9jWKUOJs
+{
+	/// <summary>
+	/// Description of sDefault.
+	/// </summary>
+	public partial class sDefault : NxtControl.GuiFramework.HMISymbol
+	{
+		public sDefault()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
