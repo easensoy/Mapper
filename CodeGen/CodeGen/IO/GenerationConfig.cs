22dd961143d22e1d8b242ba0fa11e4c9efbf0e64
easensoy <eminalpersensoy@gmail.com>
2026-06-20 09:04:46 +0100

    Add CodeGen/CodeGen/IO/GenerationConfig.cs

---
+++ CodeGen/CodeGen/IO/GenerationConfig.cs
@@ -0,0 +1,14 @@
+namespace CodeGen.Configuration
+{
+    public sealed class GenerationConfig
+    {
+        public int RecipeArraySize { get; set; }
+        public int InterlockRuleCap { get; set; }
+        public int DefaultMotionMs { get; set; }
+        public int CoverMotionMs { get; set; }
+
+        private static readonly YamlConfigFile<GenerationConfig> _file = new("Config", "config.yaml");
+
+        public static GenerationConfig Current => _file.Load();
+    }
+}
