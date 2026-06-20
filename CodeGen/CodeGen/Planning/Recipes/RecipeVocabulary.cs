1b359a83b30ddab4afd9201644ca745f8fb54b8b
easensoy <eminalpersensoy@gmail.com>
2026-06-20 09:04:53 +0100

    Add CodeGen/CodeGen/Planning/Recipes/RecipeVocabulary.cs

---
+++ CodeGen/CodeGen/Planning/Recipes/RecipeVocabulary.cs
@@ -0,0 +1,9 @@
+namespace CodeGen.Translation.Process
+{
+    internal static class StepType
+    {
+        public const int Cmd = 1;
+        public const int Wait = 2;
+        public const int End = 9;
+    }
+}
