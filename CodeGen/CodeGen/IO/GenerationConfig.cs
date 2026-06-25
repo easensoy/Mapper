80c06f602d01248cd1ce534099e6cb34bf7cc9bd
easensoy <eminalpersensoy@gmail.com>
2026-06-26 00:32:11 +0100

    GenerationConfig: add CoverGripperAckMs

--- CodeGen/CodeGen/IO/GenerationConfig.cs
+++ CodeGen/CodeGen/IO/GenerationConfig.cs
@@ -5,6 +5,7 @@ namespace CodeGen.Configuration
         public int RecipeArraySize { get; set; }
         public int DefaultMotionMs { get; set; }
         public int CoverMotionMs { get; set; }
+        public int CoverGripperAckMs { get; set; }
 
         private static readonly YamlConfigFile<GenerationConfig> _file = new("Config", "config.yaml");
 
