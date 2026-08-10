16e6e24384016f686d816b947d7c4b2be988d2f6
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:26 +0100

    Add the Seven_State_Actuator_Centre_Home_CAT faceplate source Seven_State_Actuator_Centre_Home_CAT_setup.cnv.Designer.cs

---
+++ Template Library/HMI/Faceplates/Seven_State_Actuator_Centre_Home_CAT/Seven_State_Actuator_Centre_Home_CAT_setup.cnv.Designer.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 3/25/2026
+ * Time: 9:32 AM
+ * 
+ */
+using System;
+using System.ComponentModel;
+using System.Collections;
+using System.Diagnostics;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Faceplates.Seven_State_Actuator_Centre_Home_CAT
+{
+	/// <summary>
+	/// Summary description for setup.
+	/// </summary>
+	partial class setup
+	{
+
+		#region Component Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// setup
+			// 
+			this.Bounds = new NxtControl.Drawing.RectF(((float)(0D)), ((float)(0D)), ((float)(112D)), ((float)(88D)));
+			this.Brush = new NxtControl.Drawing.Brush("FaceplateBrush");
+			this.Size = new System.Drawing.Size(112, 88);
+
+		}
+		#endregion
+	}
+}
