51818a8ebfece79499553955f8de20fdf061d6b1
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:21 +0100

    Add the Sensor_Bool_CAT faceplate source Sensor_Bool_CAT.def.cs

---
+++ Template Library/HMI/Faceplates/Sensor_Bool_CAT/Sensor_Bool_CAT.def.cs
@@ -0,0 +1,11 @@
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+
+#region Definitions;
+#region Sensor_Bool_CAT_HMI;
+#endregion Sensor_Bool_CAT_HMI;
+
+#endregion Definitions;
+
