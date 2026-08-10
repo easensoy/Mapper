66c78fa35563d1873d7393f74059540c7d897bb4
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:16 +0100

    Add the Process1_Generic faceplate source Process1_Generic.def.cs

---
+++ Template Library/HMI/Faceplates/Process1_Generic/Process1_Generic.def.cs
@@ -0,0 +1,11 @@
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+
+#region Definitions;
+#region Process1_Generic_HMI;
+#endregion Process1_Generic_HMI;
+
+#endregion Definitions;
+
