ee8bc730747112abd14529b05fd9425603cce5a5
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:22 +0100

    Add the Sensor_Bool_CAT faceplate source Sensor_Bool_CAT.event.cs

---
+++ Template Library/HMI/Faceplates/Sensor_Bool_CAT/Sensor_Bool_CAT.event.cs
@@ -0,0 +1,120 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 2/2/2026
+ * Time: 4:04 PM
+ * 
+ */
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+#region Definitions;
+#region #Sensor_Bool_CAT_HMI;
+
+namespace HMI.Main.Symbols.Sensor_Bool_CAT
+{
+
+  public class REQEventArgs : System.EventArgs
+  {
+    IHMIAccessorService accessorService;
+    int channelId;
+    int cookie; 
+    int eventIndex;
+
+    public REQEventArgs(int channelId, int cookie, int eventIndex)
+    {
+      this.accessorService = (IHMIAccessorService)ServiceProvider.GetService(typeof(IHMIAccessorService));
+      this.channelId = channelId;
+      this.cookie = cookie;
+      this.eventIndex = eventIndex;
+    }
+    public bool Get_State(ref System.Boolean value)
+    {
+      if (accessorService == null)
+        return false;
+      bool var = false;
+      bool ret = accessorService.GetBoolValue(channelId, cookie, eventIndex, true,0, ref var);
+      if (ret) value = (System.Boolean) var;
+      return ret;
+    }
+
+    public System.Boolean? State
+    { get {
+      if (accessorService == null)
+        return null;
+      bool var = false;
+      bool ret = accessorService.GetBoolValue(channelId, cookie, eventIndex, true,0, ref var);
+      if (!ret) return null;
+      return (System.Boolean) var;
+    }  }
+
+    public bool Get_name(ref System.String value)
+    {
+      if (accessorService == null)
+        return false;
+      string var = null;
+      bool ret = accessorService.GetStringValue(channelId, cookie, eventIndex, true,1, ref var);
+      if (ret) value = (System.String) var;
+      return ret;
+    }
+
+    public System.String name
+    { get {
+      if (accessorService == null)
+        return null;
+      string var = null;
+      bool ret = accessorService.GetStringValue(channelId, cookie, eventIndex, true,1, ref var);
+      if (!ret) return null;
+      return (System.String) var;
+    }  }
+
+
+  }
+
+}
+
+namespace HMI.Main.Symbols.Sensor_Bool_CAT
+{
+  partial class sDefault
+  {
+
+    private event EventHandler<HMI.Main.Symbols.Sensor_Bool_CAT.REQEventArgs> REQ_Fired;
+
+    protected override void OnEndInit()
+    {
+      if (REQ_Fired != null)
+        AttachEventInput(0);
+
+    }
+
+    protected override void FireEventCallback(int channelId, int cookie, int eventIndex)
+    {
+      switch(eventIndex)
+      {
+        default:
+          break;
+        case 0:
+          if (REQ_Fired != null)
+          {
+            try
+            {
+              REQ_Fired(this, new HMI.Main.Symbols.Sensor_Bool_CAT.REQEventArgs(channelId, cookie, eventIndex));
+            }
+            catch (System.Exception e)
+            {
+              NxtControl.Services.LoggingService.ErrorFormatted(@"In Event Callback for event:'{0}' Type:'{1}' CAT:'{2}' came exception:{3}
+stack Trace:
+{4}","REQ_Fired", this.GetType().Name, this.CATName, e.Message, e.StackTrace);
+            }
+          }
+        break; 
+
+      }
+    }
+
+  }
+}
+#endregion #Sensor_Bool_CAT_HMI;
+
+#endregion Definitions;
