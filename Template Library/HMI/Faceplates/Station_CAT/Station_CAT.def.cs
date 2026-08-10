b9465a2a46866606d6b5f0bed731607db26541af
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:27 +0100

    Add the Station_CAT faceplate source Station_CAT.def.cs

---
+++ Template Library/HMI/Faceplates/Station_CAT/Station_CAT.def.cs
@@ -0,0 +1,11 @@
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+
+#region Definitions;
+#region Station_CAT_HMI;
+#endregion Station_CAT_HMI;
+
+#endregion Definitions;
+
