112f2ffd80bc28cec56a7ee8ac3c78cf8d19f688
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:20 +0100

    Add the Robot_Task_CAT faceplate source Robot_Task_CAT_sDefault.cnv.cs

---
+++ Template Library/HMI/Faceplates/Robot_Task_CAT/Robot_Task_CAT_sDefault.cnv.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 10/8/2025
+ * Time: 11:54 AM
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.Robot_Task_CAT
+{
+	/// <summary>
+	/// Description of sDefault.
+	/// </summary>
+	public partial class sDefault : NxtControl.GuiFramework.HMISymbol
+	{
+		public sDefault()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
