a43d91b59511e46b4025f71b5309ee7fb00a5bdb
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:17 +0100

    Add the Process1_Generic faceplate source Process1_Generic_sDefault.cnv.cs

---
+++ Template Library/HMI/Faceplates/Process1_Generic/Process1_Generic_sDefault.cnv.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 9/29/2025
+ * Time: 10:14 AM
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.Process1_Generic
+{
+	/// <summary>
+	/// Description of sDefault.
+	/// </summary>
+	public partial class sDefault : NxtControl.GuiFramework.HMISymbol
+	{
+		public sDefault()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
