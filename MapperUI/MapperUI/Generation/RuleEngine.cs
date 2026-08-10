1692a54b2b0d15fe95e295e8942ebfae05765840
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:24:57 +0100

    Remove the unused global alias

--- MapperUI/MapperUI/Generation/RuleEngine.cs
+++ MapperUI/MapperUI/Generation/RuleEngine.cs
@@ -1,3 +1,2 @@
-global using MappingType = CodeGen.Models.MappingType;
 global using MappingRuleEntry = CodeGen.Models.MappingRuleEntry;
 global using MappingRuleEngine = CodeGen.Translation.MappingRuleEngine;
