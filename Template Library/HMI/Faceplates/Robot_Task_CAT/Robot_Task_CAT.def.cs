41f8bbca613f80a01cff24dabc5873dd6d189e2d
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:18 +0100

    Add the Robot_Task_CAT faceplate source Robot_Task_CAT.def.cs

---
+++ Template Library/HMI/Faceplates/Robot_Task_CAT/Robot_Task_CAT.def.cs
@@ -0,0 +1,14 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 10/8/2025
+ * Time: 11:54 AM
+ * 
+ */
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+#region Definitions;
+
+#endregion Definitions;
