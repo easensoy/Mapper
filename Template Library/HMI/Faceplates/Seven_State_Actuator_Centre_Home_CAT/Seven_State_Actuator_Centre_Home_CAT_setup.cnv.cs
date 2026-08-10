0d66993c569ad6eadad055a02084707c444988b1
easensoy <eminalpersensoy@gmail.com>
2026-08-10 10:25:26 +0100

    Add the Seven_State_Actuator_Centre_Home_CAT faceplate source Seven_State_Actuator_Centre_Home_CAT_setup.cnv.cs

---
+++ Template Library/HMI/Faceplates/Seven_State_Actuator_Centre_Home_CAT/Seven_State_Actuator_Centre_Home_CAT_setup.cnv.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 3/25/2026
+ * Time: 9:32 AM
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Faceplates.Seven_State_Actuator_Centre_Home_CAT
+{
+	/// <summary>
+	/// Description of setup.
+	/// </summary>
+	public partial class setup : NxtControl.GuiFramework.HMIFaceplate
+	{
+		public setup()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
