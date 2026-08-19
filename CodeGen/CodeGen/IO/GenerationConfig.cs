57ff54becab533be64a49d404e361e0bc6b1278e
easensoy <eminalpersensoy@gmail.com>
2026-08-19 12:46:12 +0300

    Drop the superseded gripper ack setting

--- CodeGen/CodeGen/IO/GenerationConfig.cs
+++ CodeGen/CodeGen/IO/GenerationConfig.cs
@@ -5,7 +5,6 @@
         public int RecipeArraySize { get; set; }
         public int DefaultMotionMs { get; set; }
         public int CoverMotionMs { get; set; }
-        public int CoverGripperAckMs { get; set; }
         public int BearingPnpHomeBrakeMs { get; set; }
 
         // Attempts for a generated-file write that EAE may be holding open.
