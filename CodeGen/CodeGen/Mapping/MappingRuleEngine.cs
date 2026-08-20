4fe728dc873deb46be4735facf5339842181ca3c
easensoy <eminalpersensoy@gmail.com>
2026-08-20 16:10:53 +0300

    Update MappingRuleEngine for the generic-compiler phase

--- CodeGen/CodeGen/Mapping/MappingRuleEngine.cs
+++ CodeGen/CodeGen/Mapping/MappingRuleEngine.cs
@@ -1,8 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Xml.Linq;
 using CodeGen.Models;
 
