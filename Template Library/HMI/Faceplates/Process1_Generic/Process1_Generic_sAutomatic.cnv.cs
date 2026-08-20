0bf1a05fe0d455910f2ca62ec216113fa09a91de
easensoy <eminalpersensoy@gmail.com>
2026-08-20 16:11:07 +0300

    Stage Process1_Generic_sAutomatic.cnv.cs for the panel

---
+++ Template Library/HMI/Faceplates/Process1_Generic/Process1_Generic_sAutomatic.cnv.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User:  
+ * Date: 7/14/2026
+ * Time: 3:31 PM
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.Process1_Generic
+{
+	/// <summary>
+	/// Description of Symbol3.
+	/// </summary>
+	public partial class sAutomatic : NxtControl.GuiFramework.HMISymbol
+	{
+		public sAutomatic()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
