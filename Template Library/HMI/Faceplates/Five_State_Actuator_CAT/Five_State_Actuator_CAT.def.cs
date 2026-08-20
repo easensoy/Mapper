96d0375761be8030111cd84c6dff48c15bf9c0fa
easensoy <eminalpersensoy@gmail.com>
2026-08-20 16:11:00 +0300

    Follow the per-action command model in Five_State_Actuator_CAT.def.cs

--- Template Library/HMI/Faceplates/Five_State_Actuator_CAT/Five_State_Actuator_CAT.def.cs
+++ Template Library/HMI/Faceplates/Five_State_Actuator_CAT/Five_State_Actuator_CAT.def.cs
@@ -37,41 +37,12 @@ namespace HMI.Main.Symbols.Five_State_Actuator_CAT
       }
     }
      
-    private HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock sInterlock
-    {
-      get
-      { 
-        if (IsOpenFaceplateSecure() == false)
-          return null;
-
-        HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock faceplate = null;
-        
-        IHMIManagementService hmiManagementService = (IHMIManagementService)ServiceProvider.GetService(typeof(IHMIManagementService));
-        if (hmiManagementService != null)
-          faceplate = (HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock)hmiManagementService.GetRegisteredHMIFaceplate(MapPath, typeof(HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock));
-        
-        if (faceplate == null)
-        {
-          faceplate = new HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock();
-
-          faceplate.SetConnectionInfo(this.TagName, this.SymbolPath, this.ChannelId, GetType());
-
-          if (hmiManagementService != null)
-            hmiManagementService.RegisterHMIFaceplate(faceplate);
-        }
-        return faceplate;
-      }
-    }
-     
     protected override void DoOpenFaceplate(OpenFaceplate openFaceplate)
     {
       NxtControl.GuiFramework.HMIFaceplate hmiFaceplate = null;
 
       if ("sFault" == (string)openFaceplate.FaceplateType)
         hmiFaceplate = sFault;
-
-      if ("sInterlock" == (string)openFaceplate.FaceplateType)
-        hmiFaceplate = sInterlock;
 
       if (hmiFaceplate != null)
       {
@@ -91,9 +62,6 @@ namespace HMI.Main.Symbols.Five_State_Actuator_CAT
 
       if ("sFault" == openFaceplate)
         hmiFaceplate = sFault;
-
-      if ("sInterlock" == openFaceplate)
-        hmiFaceplate = sInterlock;
 
       if (hmiFaceplate != null)
       {
@@ -133,182 +101,6 @@ namespace HMI.Main.Symbols.Five_State_Actuator_CAT
           faceplate = new HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault();
 
           faceplate.SetConnectionInfo(this.TagName, this.SymbolPath, this.ChannelId, GetType());
-
-          if (hmiManagementService != null)
-            hmiManagementService.RegisterHMIFaceplate(faceplate);
-        }
-        return faceplate;
-      }
-    }
-     
-    private HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock sInterlock
-    {
-      get
-      { 
-        if (IsOpenFaceplateSecure() == false)
-          return null;
-
-        HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock faceplate = null;
-        
-        IHMIManagementService hmiManagementService = (IHMIManagementService)ServiceProvider.GetService(typeof(IHMIManagementService));
-        if (hmiManagementService != null)
-          faceplate = (HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock)hmiManagementService.GetRegisteredHMIFaceplate(MapPath, typeof(HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock));
-        
-        if (faceplate == null)
-        {
-          faceplate = new HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock();
-
-          faceplate.SetConnectionInfo(this.TagName, this.SymbolPath, this.ChannelId, GetType());
-
-          if (hmiManagementService != null)
-            hmiManagementService.RegisterHMIFaceplate(faceplate);
-        }
-        return faceplate;
-      }
-    }
-     
-    protected override void DoOpenFaceplate(OpenFaceplate openFaceplate)
-    {
-      NxtControl.GuiFramework.HMIFaceplate hmiFaceplate = null;
-
-      if ("sFault" == (string)openFaceplate.FaceplateType)
-        hmiFaceplate = sFault;
-
-      if ("sInterlock" == (string)openFaceplate.FaceplateType)
-        hmiFaceplate = sInterlock;
-
-      if (hmiFaceplate != null)
-      {
-        if (hmiFaceplate.Initialized == true)
-          hmiFaceplate.Activate();
-        else
-        {
-          OnInitializeFaceplate(hmiFaceplate);
-          hmiFaceplate.Show(this);
-        }
-      }
-    }
-
-    public override void DoOpenFaceplate(string openFaceplate)
-    {
-      NxtControl.GuiFramework.HMIFaceplate hmiFaceplate = null;
-
-      if ("sFault" == openFaceplate)
-        hmiFaceplate = sFault;
-
-      if ("sInterlock" == openFaceplate)
-        hmiFaceplate = sInterlock;
-
-      if (hmiFaceplate != null)
-      {
-        if (hmiFaceplate.Initialized == true)
-          hmiFaceplate.Activate();
-        else
-        {
-          OnInitializeFaceplate(hmiFaceplate);
-          hmiFaceplate.Show(this);
-        }
-      }
-    }
-
-  }
-}
-
-namespace HMI.Main.Faceplates.Five_State_Actuator_CAT
-{
-  partial class sFault
-  {
-
-    private HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock sInterlock
-    {
-      get
-      { 
-        if (IsOpenFaceplateSecure() == false)
-          return null;
-
-        HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock faceplate = null;
-        
-        IHMIManagementService hmiManagementService = (IHMIManagementService)ServiceProvider.GetService(typeof(IHMIManagementService));
-        if (hmiManagementService != null)
-          faceplate = (HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock)hmiManagementService.GetRegisteredHMIFaceplate(MapPath, typeof(HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock));
-        
-        if (faceplate == null)
-        {
-          faceplate = new HMI.Main.Faceplates.Five_State_Actuator_CAT.sInterlock();
-
-          faceplate.SetConnectionInfo(this.TagName, this.ConnectionSymbolPath, this.ChannelId, this.ParentType);
-
-          if (hmiManagementService != null)
-            hmiManagementService.RegisterHMIFaceplate(faceplate);
-        }
-        return faceplate;
-      }
-    }
-     
-    protected override void DoOpenFaceplate(OpenFaceplate openFaceplate)
-    {
-      NxtControl.GuiFramework.HMIFaceplate hmiFaceplate = null;
-
-      if ("sInterlock" == (string)openFaceplate.FaceplateType)
-        hmiFaceplate = sInterlock;
-
-      if (hmiFaceplate != null)
-      {
-        if (hmiFaceplate.Initialized == true)
-          hmiFaceplate.Activate();
-        else
-        {
-          OnInitializeFaceplate(hmiFaceplate);
-          hmiFaceplate.Show(this);
-        }
-      }
-    }
-
-    public override void DoOpenFaceplate(string openFaceplate)
-    {
-      NxtControl.GuiFramework.HMIFaceplate hmiFaceplate = null;
-
-      if ("sInterlock" == openFaceplate)
-        hmiFaceplate = sInterlock;
-
-      if (hmiFaceplate != null)
-      {
-        if (hmiFaceplate.Initialized == true)
-          hmiFaceplate.Activate();
-        else
-        {
-          OnInitializeFaceplate(hmiFaceplate);
-          hmiFaceplate.Show(this);
-        }
-      }
-    }
-
-  }
-}
-
-namespace HMI.Main.Faceplates.Five_State_Actuator_CAT
-{
-  partial class sInterlock
-  {
-
-    private HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault sFault
-    {
-      get
-      { 
-        if (IsOpenFaceplateSecure() == false)
-          return null;
-
-        HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault faceplate = null;
-        
-        IHMIManagementService hmiManagementService = (IHMIManagementService)ServiceProvider.GetService(typeof(IHMIManagementService));
-        if (hmiManagementService != null)
-          faceplate = (HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault)hmiManagementService.GetRegisteredHMIFaceplate(MapPath, typeof(HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault));
-        
-        if (faceplate == null)
-        {
-          faceplate = new HMI.Main.Faceplates.Five_State_Actuator_CAT.sFault();
-
-          faceplate.SetConnectionInfo(this.TagName, this.ConnectionSymbolPath, this.ChannelId, this.ParentType);
 
           if (hmiManagementService != null)
             hmiManagementService.RegisterHMIFaceplate(faceplate);
