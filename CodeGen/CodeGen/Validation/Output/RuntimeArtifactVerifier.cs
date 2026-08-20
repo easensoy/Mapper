40e7e8269ac320e1865a9d5d2a3dc6a46c76a320
easensoy <eminalpersensoy@gmail.com>
2026-08-20 16:10:58 +0300

    Update RuntimeArtifactVerifier for the generic-compiler phase

--- CodeGen/CodeGen/Validation/Output/RuntimeArtifactVerifier.cs
+++ CodeGen/CodeGen/Validation/Output/RuntimeArtifactVerifier.cs
@@ -1,7 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.IO;
 using System.Xml.Linq;
 using CodeGen.Configuration;
 using CodeGen.Devices.Core;
