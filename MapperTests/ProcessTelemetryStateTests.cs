bfea2225bb5f418c2f172dc559f40767c4a5b0a1
easensoy <eminalpersensoy@gmail.com>
2026-08-26 03:49:31 +0300

    Cover the behaviour this refactor made explicit

--- MapperTests/ProcessTelemetryStateTests.cs
+++ MapperTests/ProcessTelemetryStateTests.cs
@@ -40,7 +40,7 @@ namespace MapperTests
         private static (RecipeArrays Arrays, VueOneComponent Process)
             Compile(string suffix, string processName)
         {
-            var ctx = GenerationContext.Plan(new MapperConfig(), ModelPath(suffix), DeploymentProfile.M262Only(LayoutCatalog.Load()));
+            var ctx = GenerationContext.Plan(TestConfig.Cfg, ModelPath(suffix), DeploymentProfile.AsPlaced(TestConfig.Cfg));
             var process = ctx.Components.Single(c =>
                 string.Equals(c.Type, "Process", StringComparison.OrdinalIgnoreCase) &&
                 string.Equals(c.Name, processName, StringComparison.OrdinalIgnoreCase));
